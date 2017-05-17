da1fe918630b41c6afb0f15f2d3cf8f1ab450b33
sumnee <sumneerijal@gmail.com>
2017-05-17 14:12:49 +0545

    project init

---
+++ product/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Product
+
+class ProductListView(ListView):
+	model=Product
+	template_name='product.html'
+
+	def get_context_data(self,**kwargs):
+		context=super(ProductListView,self).get_context_data(*kwargs)
+		context['product']=Product.objects.all()
+		return context
+
+
+# Create your views here.
